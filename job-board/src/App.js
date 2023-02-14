7d9cb0683db061dd5d774e1cdaafe8982848aae0
jay mbans <jordan@mbanefo.com>
2023-02-14 00:14:03 -0500

    component boilerplate

--- job-board/src/App.js
+++ job-board/src/App.js
@@ -1,9 +1,17 @@
-import './style.css'
+import './style.css';
+import { Routes, Route, Link } from 'react-router-dom';
+import JobBoard from './components/JobBoard'
+import Login from './components/Login'
+import JobDetails from './components/JobDetails'
 
 function App() {
   return (
     <div className="App">
-      test
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/jobs" element={<JobBoard />} />
+        <Route path="/job/:id" element={<JobDetails />} />
+      </Routes>
     </div>
   );
 }
